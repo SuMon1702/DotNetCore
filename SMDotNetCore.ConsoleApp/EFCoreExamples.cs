8556bdd95ee02ae33fefa359bf5f50d52c132f20
sg788 <sumangtm1083@gmail.com>
2024-12-19 23:32:27 +0630

    Edit

--- SMDotNetCore.ConsoleApp/EFCoreExamples.cs
+++ SMDotNetCore.ConsoleApp/EFCoreExamples.cs
@@ -8,14 +8,17 @@ namespace SMDotNetCore.ConsoleApp
 {
     internal class EFCoreExamples
     {
+        private readonly AppDbContext db = new AppDbContext();
         public void Run()
         {
             Read();
+            Edit(5);
+            Edit(25);
         }
 
         private void Read()
         {
-            AppDbContext db = new AppDbContext();
+            
             var lst = db.Movies.ToList();
             foreach (MovieModel item in lst)
             {
@@ -26,6 +29,21 @@ namespace SMDotNetCore.ConsoleApp
                 Console.WriteLine("...........................");
 
             }
+        }
+
+        private void Edit(int id)
+        {
+            var item =db.Movies.FirstOrDefault(x => x.MovieID == id);
+            if(item is null)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+
+            Console.WriteLine(item.MovieID);
+            Console.WriteLine(item.MovieName);
+            Console.WriteLine(item.MovieTitle);
+            Console.WriteLine(item.MovieContent);
         }
 
 
