907644d2f9d9b599b08d83eb6e6ab086a6effc94
sg788 <sumangtm1083@gmail.com>
2024-12-19 22:53:04 +0630

    Read

---
+++ SMDotNetCore.ConsoleApp/EFCoreExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleApp
+{
+    internal class EFCoreExamples
+    {
+        public void Run()
+        {
+            Read();
+        }
+
+        private void Read()
+        {
+            AppDbContext db = new AppDbContext();
+            var lst = db.Movies.ToList();
+            foreach (MovieModel item in lst)
+            {
+                Console.WriteLine(item.MovieID);
+                Console.WriteLine(item.MovieName);
+                Console.WriteLine(item.MovieTitle);
+                Console.WriteLine(item.MovieContent);
+                Console.WriteLine("...........................");
+
+            }
+        }
+
+
+    }
+}
