66585f02dcf5009eb041f794f452f581b97cc562
sg788 <sumangtm1083@gmail.com>
2024-12-19 22:51:43 +0630

    -

--- SMDotNetCore.ConsoleApp/Program.cs
+++ SMDotNetCore.ConsoleApp/Program.cs
@@ -16,5 +16,8 @@ AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 // adoDotNetExample.Edit(14);
 // adoDotNetExample.Edit(1);
 
-DapperExamples dapperExamples = new DapperExamples();
-dapperExamples.Run();
+//DapperExamples dapperExamples = new DapperExamples();
+//dapperExamples.Run();
+
+EFCoreExamples eFCoreExamples = new EFCoreExamples();
+eFCoreExamples.Run();
