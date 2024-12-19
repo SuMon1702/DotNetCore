70173522052820773a606f2da86c7b71694f8a99
sg788 <sumangtm1083@gmail.com>
2024-12-19 22:25:03 +0630

    -

---
+++ SMDotNetCore.ConsoleApp/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleApp
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        public DbSet<MovieModel> Movies { get; set; }
+
+
+    }
+}
