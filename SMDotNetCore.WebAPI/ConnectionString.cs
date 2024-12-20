9dcdc77c72b105bc674a5038cc90969a2cbaec56
sg788 <sumangtm1083@gmail.com>
2024-12-20 19:44:50 +0630

    -

---
+++ SMDotNetCore.WebAPI/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+namespace SMDotNetCore.WebAPI
+{
+    internal static class ConnectionString
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "SMDotNetTrainingDB",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
