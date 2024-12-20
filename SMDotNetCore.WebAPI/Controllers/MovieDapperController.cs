503c044e5e1ff84152a4746a10aaf35c1b28c026
sg788 <sumangtm1083@gmail.com>
2024-12-20 23:44:30 +0630

    Read

---
+++ SMDotNetCore.WebAPI/Controllers/MovieDapperController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SMDotNetCore.WebAPI.Model;
+using System.Data;
+
+namespace SMDotNetCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieDapperController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            string query= "select * from Tbl_Movie";
+            using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+            List<MovieModel> lst = db.Query<MovieModel>(query).ToList();
+            return Ok(lst);
+
+        }
+
+       
+    }
+}
