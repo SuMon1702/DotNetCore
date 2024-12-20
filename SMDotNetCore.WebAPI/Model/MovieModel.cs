3b653a6f4167f37ba079ca4a6479a59d32ecf9b7
sg788 <sumangtm1083@gmail.com>
2024-12-20 19:44:40 +0630

    Model

---
+++ SMDotNetCore.WebAPI/Model/MovieModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMDotNetCore.WebAPI.Model
+{
+    [Table("Tbl_Movie")]
+    public class MovieModel
+    {
+        [Key]
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieContent { get; set; }
+
+    }
+}
