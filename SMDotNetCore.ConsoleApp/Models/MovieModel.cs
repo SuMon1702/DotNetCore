3c63d4ac7eff7c1fc19e98a681b0c9efc665c649
sg788 <sumangtm1083@gmail.com>
2024-12-20 15:39:25 +0630

    Models

---
+++ SMDotNetCore.ConsoleApp/Models/MovieModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleApp.Models
+{
+    [Table("Tbl_Movie")]
+    public class MovieModel
+    {
+        [Key]
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieContent { get; set; }
+
+    }
+}
