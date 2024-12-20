41b23fbf0ae46da7b000b830bd7cfcda96ec4166
sg788 <sumangtm1083@gmail.com>
2024-12-20 22:03:25 +0630

    Update[Patch]

--- SMDotNetCore.WebAPI/Model/MovieModel.cs
+++ SMDotNetCore.WebAPI/Model/MovieModel.cs
@@ -8,9 +8,9 @@ namespace SMDotNetCore.WebAPI.Model
     {
         [Key]
         public int MovieID { get; set; }
-        public string MovieName { get; set; }
-        public string MovieTitle { get; set; }
-        public string MovieContent { get; set; }
+        public string? MovieName { get; set; }
+        public string? MovieTitle { get; set; }
+        public string? MovieContent { get; set; }
 
     }
 }
