af20dea8666ce3bb3d8323e793538285b822b338
sg788 <sumangtm1083@gmail.com>
2024-12-20 20:25:36 +0630

    AppDbContext

--- SMDotNetCore.WebAPI/AppDbContext/AppDbContext.cs
+++ SMDotNetCore.WebAPI/AppDbContext/AppDbContext.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SMDotNetCore.WebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SMDotNetCore.WebAPI.AppDbContext
+namespace SMDotNetCore.WebAPI;
+
+internal class AppDbContext : DbContext
 {
-    internal class AppDbContext:DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
-        }
-
-        public DbSet<MovieModel> Movies { get; set; }
+        optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
     }
+
+    public DbSet<MovieModel> Movies { get; set; }
+
+
 }
