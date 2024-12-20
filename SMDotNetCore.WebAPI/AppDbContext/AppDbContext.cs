27877b78a8fa73b2f84fa623fe6d37168615227b
sg788 <sumangtm1083@gmail.com>
2024-12-20 19:44:09 +0630

    AppDbContext

---
+++ SMDotNetCore.WebAPI/AppDbContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMDotNetCore.WebAPI.Model;
+
+namespace SMDotNetCore.WebAPI.AppDbContext
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<MovieModel> Movies { get; set; }
+    }
+}
