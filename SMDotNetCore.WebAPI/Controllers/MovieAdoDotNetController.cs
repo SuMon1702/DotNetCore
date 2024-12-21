08d1b4d0ad59318c31a5631a4f1b65df35a504e5
sg788 <sumangtm1083@gmail.com>
2024-12-21 13:24:31 +0630

    Edit

--- SMDotNetCore.WebAPI/Controllers/MovieAdoDotNetController.cs
+++ SMDotNetCore.WebAPI/Controllers/MovieAdoDotNetController.cs
@@ -5,6 +5,7 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using SMDotNetCore.WebAPI.Model;
 
+
 namespace SMDotNetCore.WebAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -46,6 +47,36 @@ namespace SMDotNetCore.WebAPI.Controllers
             }).ToList();
 
             return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetMovie(int id)
+        {
+            string query = "Select * from Tbl_Movie Where MovieID= @MovieID";
+            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@MovieID", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                return NotFound("No data Found");
+            }
+
+            DataRow dr = dt.Rows[0];
+            var item = new MovieModel
+            {
+                MovieID = Convert.ToInt32(dr["MovieID"]),
+                MovieName = Convert.ToString(dr["MovieName"]),
+                MovieTitle = Convert.ToString(dr["MovieTitle"]),
+                MovieContent = Convert.ToString(dr["MovieContent"])
+            };
+            return Ok(item);
         }
     }
 }
