516f383f4333a9aa9b5e53781ee26df816224566
sg788 <sumangtm1083@gmail.com>
2024-12-27 23:58:05 +0630

    -

--- SMDotNetCore.RestApiWithNLayer/Features/Movie/BL_Movie.cs
+++ SMDotNetCore.RestApiWithNLayer/Features/Movie/BL_Movie.cs
@@ -1,6 +1,14 @@
-﻿namespace SMDotNetCore.RestApiWithNLayer.Features.Movie
+﻿using SMDotNetCore.RestApiWithNLayer.Features.Blog;
+
+namespace SMDotNetCore.RestApiWithNLayer.Features.Movie
 {
     public class BL_Movie
     {
+        private readonly DA_Movie _daMovie;
+
+        public BL_Movie()
+        {
+            _daMovie = new DA_Movie();
+        }
     }
 }
