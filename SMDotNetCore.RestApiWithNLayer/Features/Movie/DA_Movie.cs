2373925a537cd19435628ba45da1775cc09d6585
sg788 <sumangtm1083@gmail.com>
2024-12-27 23:53:18 +0630

    -

--- SMDotNetCore.RestApiWithNLayer/Features/Movie/DA_Movie.cs
+++ SMDotNetCore.RestApiWithNLayer/Features/Movie/DA_Movie.cs
@@ -1,7 +1,27 @@
-﻿namespace SMDotNetCore.RestApiWithNLayer.Features.Blog
+﻿
+using SMDotNetCore.RestApiWithNLayer.Model;
+using System.Collections.Generic;
+using SMDotNetCore.RestApiWithNLayer.DB;
+
+
+namespace SMDotNetCore.RestApiWithNLayer.Features.Blog
 {
     public class DA_Movie
     {
+        private readonly AppDbContext _appDbContext;
 
+        public DA_Movie()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        public List<MovieModel> GetMovies()
+        {
+            var lst= _appDbContext.Movies.ToList();
+            return lst;
+        }
+
+        
     }
+
 }
