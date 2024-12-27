5411ceb0881ffc5689308d342f55d02fbd2eef3e
sg788 <sumangtm1083@gmail.com>
2024-12-27 23:20:01 +0630

    -

--- SMDotNetCore.WebAPI/AppDbContext/AppDbContext.cs
+++ SMDotNetCore.WebAPI/AppDbContext/AppDbContext.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SMDotNetCore.WebAPI.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMDotNetCore.WebAPI;
 
