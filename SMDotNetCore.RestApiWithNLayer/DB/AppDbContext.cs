0baaf3c777c406893d2a8e5320857cb58da0a971
sg788 <sumangtm1083@gmail.com>
2024-12-27 23:53:09 +0630

    -

---
+++ SMDotNetCore.RestApiWithNLayer/DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMDotNetCore.RestApiWithNLayer.Model;
+
+
+namespace SMDotNetCore.RestApiWithNLayer.DB;
+
+internal class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<MovieModel> Movies { get; set; }
+
+
+}
