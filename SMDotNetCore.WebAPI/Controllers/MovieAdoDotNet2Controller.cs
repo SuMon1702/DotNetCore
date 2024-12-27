0681db2f105ca76c844155590412d2bb437746c3
sg788 <sumangtm1083@gmail.com>
2024-12-27 20:33:45 +0630

    -

--- SMDotNetCore.WebAPI/Controllers/MovieAdoDotNet2Controller.cs
+++ SMDotNetCore.WebAPI/Controllers/MovieAdoDotNet2Controller.cs
@@ -5,6 +5,7 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using SMDotNetCore.WebAPI.Model;
 using SMDotNetCore.shared;
+using static SMDotNetCore.shared.AdoDotNetService;
 
 
 
@@ -38,29 +39,13 @@ namespace SMDotNetCore.WebAPI.Controllers
         public ActionResult GetMovie(int id)
         {
             string query = "Select * from Tbl_Movie Where MovieID= @MovieID";
-            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+            var item = _adoDotNetService.QueryFirstOrDefault<MovieModel>(query,new AdoDotNetParameter("@MovieID", id ));
 
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(query, connection);
-            cmd.Parameters.AddWithValue("@MovieID", id);
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sqlDataAdapter.Fill(dt);
-            connection.Close();
-
-            if (dt.Rows.Count == 0)
+            if(item is null)
             {
                 return NotFound("No data Found");
             }
 
-            DataRow dr = dt.Rows[0];
-            var item = new MovieModel
-            {
-                MovieID = Convert.ToInt32(dr["MovieID"]),
-                MovieName = Convert.ToString(dr["MovieName"]),
-                MovieTitle = Convert.ToString(dr["MovieTitle"]),
-                MovieContent = Convert.ToString(dr["MovieContent"])
-            };
             return Ok(item);
         }
 
