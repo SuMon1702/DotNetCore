33046cc5f4cdfcd4e08b8c5c0eaad3f9d274c12d
sg788 <sumangtm1083@gmail.com>
2024-12-27 20:24:56 +0630

    -

--- SMDotNetCore.shared/AdoDotNetService.cs
+++ SMDotNetCore.shared/AdoDotNetService.cs
@@ -42,9 +42,31 @@ namespace SMDotNetCore.shared
             DataTable dt = new DataTable();
             sqlDataAdapter.Fill(dt);
 
-            string json = JsonConvert.SerializeObject(dt);
-            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;
+            string json = JsonConvert.SerializeObject(dt);               // C# object to Json
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!; // Json to C# object
             return lst;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, params AdoDotNetParameter[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            using SqlCommand command = new SqlCommand(query, connection);
+            if (parameters != null && parameters.Length > 0)
+            {
+                var ParametersArray = parameters.Select(item => new SqlParameter(item.Name, item.Value)).ToArray();
+                command.Parameters.AddRange(ParametersArray);
+            }
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+
+           string json = JsonConvert.SerializeObject(dt);
+           List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;
+
+            return lst[0];
+            
         }
 
 
