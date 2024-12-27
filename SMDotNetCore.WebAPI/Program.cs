533c1edf109180a181baf107b6b00203aefd3c8d
sg788 <sumangtm1083@gmail.com>
2024-12-27 20:54:24 +0630

    -

--- SMDotNetCore.WebAPI/Program.cs
+++ SMDotNetCore.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver.Core.Configuration;
+using SMDotNetCore.shared;
+using SMDotNetCore.WebAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
