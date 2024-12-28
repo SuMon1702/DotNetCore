f74d4dc5c837b9cd21fb81d06e8707053e56358f
sg788 <sumangtm1083@gmail.com>
2024-12-28 20:17:43 +0630

    -

--- SMDotNetCore.RestApiWithNLayer/Features/Movie/MovieController.cs
+++ SMDotNetCore.RestApiWithNLayer/Features/Movie/MovieController.cs
@@ -6,5 +6,20 @@ namespace SMDotNetCore.RestApiWithNLayer.Features.Movie
     [ApiController]
     public class MovieController : ControllerBase
     {
+        private readonly BL_Movie _blMovie;
+
+        public MovieController()
+        {
+            _blMovie = new BL_Movie();
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            var lst = _blMovie.GetMovies();
+            return Ok(lst);
+        }
+
+        
     }
 }
