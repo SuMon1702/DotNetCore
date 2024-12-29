6057bd7c550e5ea3e3d6fa81dfd4210fadbfa6aa
sg788 <sumangtm1083@gmail.com>
2024-12-29 16:55:14 +0630

    [Complete]LatHtaukBayDinAPI

--- SMDotNetCore.RestApiWithNLayer/LatHtukBayDin/LHBDController.cs
+++ SMDotNetCore.RestApiWithNLayer/LatHtukBayDin/LHBDController.cs
@@ -18,7 +18,7 @@ namespace SMDotNetCore.RestApiWithNLayer.LatHtukBayDin
         [HttpGet("questions")]
         public async Task<IActionResult> Question()
         {
-            var model= await GetDataAsync();
+            var model = await GetDataAsync();
             return Ok(model.questions);
 
         }
@@ -26,30 +26,35 @@ namespace SMDotNetCore.RestApiWithNLayer.LatHtukBayDin
         [HttpGet]
         public async Task<IActionResult> NumberList()
         {
-            var model= await GetDataAsync();
+            var model = await GetDataAsync();
             return Ok(model.numberList);
         }
+
+        [HttpGet("{questionNo}/{answerNo}")]
+        public async Task<IActionResult> Answer(int questionNo, int answerNo)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == answerNo));
+        }
     }
-}
 
+    public class LatHtukBayDinModel
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
 
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
 
-public class LatHtukBayDinModel
-{
-    public Question[] questions { get; set; }
-    public Answer[] answers { get; set; }
-    public string[] numberList { get; set; }
-}
-
-public class Question
-{
-    public int questionNo { get; set; }
-    public string questionName { get; set; }
-}
-
-public class Answer
-{
-    public int questionNo { get; set; }
-    public int answerNo { get; set; }
-    public string answerResult { get; set; }
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
 }
