9b799a1661e2955f422efe9e973663d37a4f081c
sg788 <sumangtm1083@gmail.com>
2024-12-29 15:17:15 +0630

    -

--- SMDotNetCore.ConsoleAppHttpClient/Program.cs
+++ SMDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -1,7 +1,30 @@
 ﻿
 Console.WriteLine("Hello, World!");
 
-string jsonStr= await File.ReadAllTextAsync("data.json");
+string jsonStr = await File.ReadAllTextAsync("data.json");
 Console.WriteLine(jsonStr);
 
-Console.ReadKey();
+
+
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
+
