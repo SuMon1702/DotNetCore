4dad8b5f9b5a54aec0293aa8bdce446fd9d0f0fa
sg788 <sumangtm1083@gmail.com>
2024-12-29 15:17:26 +0630

    -

---
+++ SMDotNetCore.RestApiWithNLayer/LatHtukBayDin/LHBDController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMDotNetCore.RestApiWithNLayer.LatHtukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LHBDController : ControllerBase
+    {
+    }
+}
+
+
+
+public class LatHtukBayDin
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
