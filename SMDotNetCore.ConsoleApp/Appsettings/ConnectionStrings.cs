4d49173b0e1bf2893d9538d4a6e2bf1aea61b423
sg788 <sumangtm1083@gmail.com>
2024-12-31 01:09:20 +0630

    -

--- SMDotNetCore.ConsoleApp/Appsettings/ConnectionStrings.cs
+++ SMDotNetCore.ConsoleApp/Appsettings/ConnectionStrings.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
 
-namespace SMDotNetCore.ConsoleApp.Appsettings
+namespace SMDotNetCore.ConsoleApp.Appsettings;
+
+internal static class ConnectionStrings
 {
-    internal static class ConnectionStrings
+    public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
     {
-        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-        {
-            DataSource = ".",
-            InitialCatalog = "SMDotNetTrainingDB",
-            UserID = "sa",
-            Password = "sasa@123",
-            TrustServerCertificate = true
-        };
-    }
+        DataSource = ".",
+        InitialCatalog = "SMDotNetTrainingDB",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true
+    };
 }
