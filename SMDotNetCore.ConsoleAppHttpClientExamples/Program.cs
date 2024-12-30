ac7693af09eec87065d14e80dbb3d68be4556cea
sg788 <sumangtm1083@gmail.com>
2024-12-30 10:52:04 +0630

    -

--- SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
+++ SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -34,12 +34,3 @@ catch (HttpRequestException ex)
 
 
 
-public class MovieModel
-{
-    
-    public int MovieID { get; set; }
-    public string? MovieName { get; set; }
-    public string? MovieTitle { get; set; }
-    public string? MovieContent { get; set; }
-
-}
