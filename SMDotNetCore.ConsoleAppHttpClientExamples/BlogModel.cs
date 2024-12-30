62a86d3b2a318bdafd63f47433af39301ba1436d
sg788 <sumangtm1083@gmail.com>
2024-12-30 10:51:53 +0630

    -

---
+++ SMDotNetCore.ConsoleAppHttpClientExamples/BlogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleAppHttpClientExamples
+{
+    public class BlogModel
+    {
+            public int MovieID { get; set; }
+            public string? MovieName { get; set; }
+            public string? MovieTitle { get; set; }
+            public string? MovieContent { get; set; }
+
+        
+    }
+}
