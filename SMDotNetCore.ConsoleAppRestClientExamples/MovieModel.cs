3be6e18b31286c1291852527b873a541137639b7
sg788 <sumangtm1083@gmail.com>
2024-12-31 02:02:26 +0630

    -

--- SMDotNetCore.ConsoleAppRestClientExamples/MovieModel.cs
+++ SMDotNetCore.ConsoleAppRestClientExamples/MovieModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SMDotNetCore.ConsoleAppRestClientExamples
+﻿namespace SMDotNetCore.ConsoleAppRestClientExamples
 {
     public class MovieModel
     {
