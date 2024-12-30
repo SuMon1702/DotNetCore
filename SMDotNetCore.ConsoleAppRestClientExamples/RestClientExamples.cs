716327b453d8ededb8d7b5d5602b658360da60b2
sg788 <sumangtm1083@gmail.com>
2024-12-31 01:27:54 +0630

    -

---
+++ SMDotNetCore.ConsoleAppRestClientExamples/RestClientExamples.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleAppRestClientExamples
+{
+    internal class RestClientExamples
+    {
+        private readonly RestClient _restClient = new RestClient(new Uri("https://localhost:7294"));
+        private readonly string _movieEndpoint = "/api/MovieEFCore";
+
+        public async Task RunAsync()
+        {
+            
+        }
+
+    }
+}
