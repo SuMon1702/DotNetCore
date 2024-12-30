628626490824ff8b6ad655491e2457926e8e24b5
sg788 <sumangtm1083@gmail.com>
2024-12-31 00:54:50 +0630

    -

--- SMDotNetCore.shared/DapperService.cs
+++ SMDotNetCore.shared/DapperService.cs
@@ -4,7 +4,7 @@ using System.Data;
 
 namespace SMDotNetCore.shared
 {
-    
+
         public class DapperService
         {
             private readonly string _connectionString;
@@ -14,17 +14,17 @@ namespace SMDotNetCore.shared
                 _connectionString = connectionString;
             }
 
-            public List<M> Query<M>(string query, object? param = null)
+            public List<T> Query<T>(string query, object? param = null)
             {
                 using IDbConnection db = new SqlConnection(_connectionString);
-                var lst = db.Query<M>(query, param).ToList();
+                var lst = db.Query<T>(query, param).ToList();
                 return lst;
             }
 
-            public M QueryFirstOrDefault<M>(string query, object? param = null)
+            public T QueryFirstOrDefault<T>(string query, object? param = null)
             {
                 using IDbConnection db = new SqlConnection(_connectionString);
-                var item = db.Query<M>(query, param).FirstOrDefault();
+                var item = db.Query<T>(query, param).FirstOrDefault();
                 return item!;
             }
 
