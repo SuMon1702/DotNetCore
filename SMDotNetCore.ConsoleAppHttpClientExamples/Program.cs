c90e40e03cb4ae15a4c5fba18619921239f40860
sg788 <sumangtm1083@gmail.com>
2024-12-30 13:25:23 +0630

    -

--- SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
+++ SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -4,32 +4,34 @@ using System.ComponentModel.DataAnnotations;
 
 Console.WriteLine("Hello, World!");
 
-HttpClient client = new HttpClient();
-try
-{
-    var response = await client.GetAsync("https://localhost:7294/api/MovieEFCore");
-    if (response.IsSuccessStatusCode)
-    {
-        string jsonStr = await response.Content.ReadAsStringAsync();
-        List<MovieModel> lst = JsonConvert.DeserializeObject<List<MovieModel>>(jsonStr)!;
+//HttpClient client = new HttpClient();
+//try
+//{
+//    var response = await client.GetAsync("https://localhost:7294/api/MovieEFCore");
+//    if (response.IsSuccessStatusCode)
+//    {
+//        string jsonStr = await response.Content.ReadAsStringAsync();
+//        List<MovieModel> lst = JsonConvert.DeserializeObject<List<MovieModel>>(jsonStr)!;
 
-        foreach (var movie in lst)
-        {
-            Console.WriteLine(JsonConvert.SerializeObject(movie));
-            Console.WriteLine($"Name=>{movie.MovieName}");
-            Console.WriteLine($"Title=>{movie.MovieTitle}");
-            Console.WriteLine($"Content=>{movie.MovieContent}");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Failed to get data");
-    }
-}
-catch (HttpRequestException ex)
-{
-    Console.WriteLine($"Request failed: {ex.Message}");
-}
+//        foreach (var movie in lst)
+//        {
+//            Console.WriteLine(JsonConvert.SerializeObject(movie));
+//            Console.WriteLine($"Name=>{movie.MovieName}");
+//            Console.WriteLine($"Title=>{movie.MovieTitle}");
+//            Console.WriteLine($"Content=>{movie.MovieContent}");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Failed to get data");
+//    }
+//}
+//catch (HttpRequestException ex)
+//{
+//    Console.WriteLine($"Request failed: {ex.Message}");
+//}
+
+
 
 
 
