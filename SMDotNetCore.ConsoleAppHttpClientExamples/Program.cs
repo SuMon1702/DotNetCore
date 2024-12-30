ceeef2f0d04553968c8810adf5064b71c1574e73
sg788 <sumangtm1083@gmail.com>
2024-12-30 10:41:13 +0630

    -

---
+++ SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+try
+{
+    var response = await client.GetAsync("https://localhost:7294/api/MovieEFCore");
+    if (response.IsSuccessStatusCode)
+    {
+        string jsonStr = await response.Content.ReadAsStringAsync();
+        List<MovieModel> lst = JsonConvert.DeserializeObject<List<MovieModel>>(jsonStr)!;
+
+        foreach (var movie in lst)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(movie));
+            Console.WriteLine($"Name=>{movie.MovieName}");
+            Console.WriteLine($"Title=>{movie.MovieTitle}");
+            Console.WriteLine($"Content=>{movie.MovieContent}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Failed to get data");
+    }
+}
+catch (HttpRequestException ex)
+{
+    Console.WriteLine($"Request failed: {ex.Message}");
+}
+
+
+
+
+public class MovieModel
+{
+    
+    public int MovieID { get; set; }
+    public string? MovieName { get; set; }
+    public string? MovieTitle { get; set; }
+    public string? MovieContent { get; set; }
+
+}
