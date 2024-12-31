c8412d17f67cb4d18ee7c85019452d17be03a749
sg788 <sumangtm1083@gmail.com>
2024-12-31 22:20:20 +0630

    -

--- SMDotNetCore.ConsoleAppRestClientExamples/Program.cs
+++ SMDotNetCore.ConsoleAppRestClientExamples/Program.cs
@@ -1,7 +1,6 @@
 ﻿using SMDotNetCore.ConsoleAppRestClientExamples;
 
 Console.WriteLine("Hello, World!");
-
 RestClientExamples restClientExamples = new RestClientExamples();
 await restClientExamples.RunAsync();
 
