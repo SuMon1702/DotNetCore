03185d92f08b5365fe5e854a919edcfe7afbbad1
sg788 <sumangtm1083@gmail.com>
2025-01-03 22:58:50 +0630

    -

---
+++ SMDotNetCore.RestApiWithNLayer/MyanmarProverbs/MyanmarProverbsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMDotNetCore.RestApiWithNLayer.MyanmarProverbs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public class Tbl_Mmproverbs
+        {
+            public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
+            public Tbl_Mmproverbs[] Tbl_MMProverbs { get; set; }
+        }
+
+        public class Tbl_Mmproverbstitle
+        {
+            public int TitleId { get; set; }
+            public string TitleName { get; set; }
+        }
+
+        public class Tbl_MmproverbsDetail
+        {
+            public int TitleId { get; set; }
+            public int ProverbId { get; set; }
+            public string ProverbName { get; set; }
+            public string ProverbDesp { get; set; }
+        }
+
+    }
+}
