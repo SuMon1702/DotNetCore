0182e46b4035ae50abab1964b3993cd506c8640a
sg788 <sumangtm1083@gmail.com>
2025-01-03 23:33:25 +0630

    -

--- SMDotNetCore.RestApiWithNLayer/MyanmarProverbs/MyanmarProverbsController.cs
+++ SMDotNetCore.RestApiWithNLayer/MyanmarProverbs/MyanmarProverbsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -25,6 +26,20 @@ namespace SMDotNetCore.RestApiWithNLayer.MyanmarProverbs
             return Ok(model.Tbl_MMProverbsTitle);
         }
 
+        [HttpGet("{titleName}")]
+        public async Task<IActionResult> Get(string titleName)
+        {
+            var model = await GetDataFromApi();
+            var item = model.Tbl_MMProverbsTitle.FirstOrDefault(x => x.TitleName == titleName);
+            if (item is null) return NotFound();
+
+            var titleId = item.TitleId;
+            var result = model.Tbl_MMProverbs.Where(x => x.TitleId == titleId);//di hti ka ko ka က yk hrr ya chin yin က ko titlename mr yite yin thu nk sine tk hrr pw lr mr
+
+            return Ok(result);
+
+
+        }
 
 
 
@@ -38,7 +53,7 @@ namespace SMDotNetCore.RestApiWithNLayer.MyanmarProverbs
         public class Tbl_Mmproverbs
         {
             public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
-            public Tbl_Mmproverbs[] Tbl_MMProverbs { get; set; }
+            public Tbl_MmproverbsDetail[] Tbl_MMProverbs { get; set; }
         }
 
         public class Tbl_Mmproverbstitle
@@ -53,6 +68,13 @@ namespace SMDotNetCore.RestApiWithNLayer.MyanmarProverbs
             public int ProverbId { get; set; }
             public string ProverbName { get; set; }
             public string ProverbDesp { get; set; }
+        }
+
+        public class Tbl_MmproverbsHead
+        {
+            public int TitleId { get; set; }
+            public int ProverbId { get; set; }
+            public string ProverbName { get; set; }
         }
 
     }
