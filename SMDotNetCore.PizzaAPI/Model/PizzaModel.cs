4029f9cc43c0bc551febf446d9867a5338ed9580
sg788 <sumangtm1083@gmail.com>
2025-01-04 23:40:33 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/Model/PizzaModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMDotNetCore.PizzaAPI.Model
+{
+    [Table("Tbl_Pizza")]
+    public class PizzaModel
+    {
+        [Key]
+        [Column("PizzaId")]
+        public int id { get; set; }
+
+        [Column("PizzaName")]
+        public string name { get; set; }
+
+        [Column("PizzaPrice")]
+        public double price { get; set; }
+
+
+    }
+}
