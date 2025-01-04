4d13e1bc7afb13c455526391234906f9720e062f
sg788 <sumangtm1083@gmail.com>
2025-01-04 23:40:43 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/Features/Pizza/PizzaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMDotNetCore.PizzaAPI.Features.Pizza
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+    }
+}
