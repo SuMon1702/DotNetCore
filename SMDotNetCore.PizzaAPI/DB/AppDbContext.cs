f07c2bfec3520bba6db9efafec3fd696d3869556
sg788 <sumangtm1083@gmail.com>
2025-01-04 23:57:46 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMDotNetCore.PizzaAPI.Model;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace SMDotNetCore.PizzaAPI.DB;
+
+internal class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<PizzaModel> Pizzas { get; set; }
+
+
+}
