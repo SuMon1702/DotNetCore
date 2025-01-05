f9cbfc71fd0f71ad358d613131920857fcd7ba01
sg788 <sumangtm1083@gmail.com>
2025-01-05 17:50:57 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/Model/PizzaOrderDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMDotNetCore.PizzaAPI.Model
+{
+    [Table("Tbl_PizzaOrderDetail")]
+    public class PizzaOrderDetailModel
+    {
+        [Key]
+        public int PizzaOrderDetailId { get; set; }
+        public required string PizzaOrderInvoiceNo { get; set; }
+        public int PizzaExtraId { get; set; }
+
+    }
+}
