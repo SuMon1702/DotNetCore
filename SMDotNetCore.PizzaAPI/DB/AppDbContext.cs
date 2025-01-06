b0d2250b8741b13ff48f2ea891f5ea3cd2b42518
sg788 <sumangtm1083@gmail.com>
2025-01-06 10:47:51 +0630

    -

--- SMDotNetCore.PizzaAPI/DB/AppDbContext.cs
+++ SMDotNetCore.PizzaAPI/DB/AppDbContext.cs
@@ -34,27 +34,6 @@ public class OrderResponse
     public decimal TotalAmount { get; set; }
 }
 
-public class PizzaOrderInvoiceDetailModel
-{
-    public int PizzaOrderDetailId { get; set; }
-    public string PizzaOrderInvoiceNo { get; set; }
-    public int PizzaExtraId { get; set; }
-    public string PizzaExtraName { get; set; }
-    public decimal Price { get; set; }
-
-}
-
-public class PizzaOrderInvoiceHeadModel
-{
-    public string PizzaOrderId { get; set; }
-    public string PizzaOrderInvoiceNo { get; set; }
-    public decimal TotalAmount { get; set; }
-    public string PizzaId { get; set; }
-    public string Pizza { get; set; }
-    public decimal Price { get; set; }
-
-}
-
 public class  PizzaOrderInvoiceResponse
 {
     public PizzaOrderInvoiceHeadModel Order { get; set; }
