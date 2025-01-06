732f6d4697baee82407dca9993737ad1232367c6
sg788 <sumangtm1083@gmail.com>
2025-01-06 12:45:15 +0630

    Interface

---
+++ SMDotNetCore.ConsoleAppRefitExamples/IMovieAPI.cs
@@ -0,0 +1,28 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleAppRefitExamples;
+
+public interface IMovieAPI
+{
+    [Get("/api/Movie")]
+    Task<List<MovieModel>> GetMovies();
+}
+
+
+
+
+public class MovieModel
+{
+    
+    public int MovieID { get; set; }
+    public string? MovieName { get; set; }
+    public string? MovieTitle { get; set; }
+    public string? MovieContent { get; set; }
+
+}
