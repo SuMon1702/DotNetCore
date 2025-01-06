bf7f2b35e486f7e00616907135199d30d1c45573
sg788 <sumangtm1083@gmail.com>
2025-01-06 12:45:34 +0630

    Read

---
+++ SMDotNetCore.ConsoleAppRefitExamples/RefitExample.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDotNetCore.ConsoleAppRefitExamples
+{
+    internal class RefitExample
+    {
+        private readonly IMovieAPI _service = RestService.For<IMovieAPI>("https://localhost:7010");
+
+        public async Task RunAsync()
+        {
+            await ReadAsync();
+        }
+
+
+        private async Task ReadAsync()
+        {
+            var lst = await _service.GetMovies();
+            foreach (var item in lst)
+            {
+                Console.WriteLine($"MovieID: {item.MovieID}");
+                Console.WriteLine($"MovieName: {item.MovieName}");
+                Console.WriteLine($"MovieTitle: {item.MovieTitle}");
+                Console.WriteLine($"MovieContent: {item.MovieContent}");
+                Console.WriteLine(".......................");
+            }
+
+        }
+    }
+}
