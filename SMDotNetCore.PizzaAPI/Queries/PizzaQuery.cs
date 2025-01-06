287517169e99982775b85b70b4552f47f56bca56
sg788 <sumangtm1083@gmail.com>
2025-01-06 09:28:29 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/Queries/PizzaQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace SMDotNetCore.PizzaAPI.Queries
+{
+    public class PizzaQuery
+    {
+        public static string PizzaOrderQuery { get; } = @"
+            SELECT 
+                po.*,
+                p.Pizza,
+                p.Price
+            FROM Tbl_PizzaOrder po
+            INNER JOIN Tbl_Pizza p ON po.PizzaId = p.PizzaId
+            WHERE po.PizzaOrderInvoiceNo = @PizzaOrderInvoiceNo;";
+    }
+
+
+}
+
+
