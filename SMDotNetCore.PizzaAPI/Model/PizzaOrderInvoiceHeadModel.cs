6691a3cdfbc577ea7ecb39878636e534643726b8
sg788 <sumangtm1083@gmail.com>
2025-01-06 09:59:54 +0630

    -

---
+++ SMDotNetCore.PizzaAPI/Model/PizzaOrderInvoiceHeadModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SMDotNetCore.PizzaAPI.Model
+{
+    public class PizzaOrderInvoiceHeadModel
+    {
+        public string PizzaOrderId { get; set; }
+        public string PizzaOrderInvoiceNo { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PizzaId { get; set; }
+        public string Pizza { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
