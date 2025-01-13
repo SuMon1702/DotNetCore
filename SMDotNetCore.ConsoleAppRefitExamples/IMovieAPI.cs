21289dd543547957a0825a7528c8d235b45d911b
sg788 <sumangtm1083@gmail.com>
2025-01-13 21:46:02 +0630

    -

--- SMDotNetCore.ConsoleAppRefitExamples/IMovieAPI.cs
+++ SMDotNetCore.ConsoleAppRefitExamples/IMovieAPI.cs
@@ -1,38 +1,33 @@
 ﻿using Refit;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SMDotNetCore.ConsoleAppRefitExamples;
-
-public interface IMovieAPI
+namespace SMDotNetCore.ConsoleAppRefitExamples
 {
-    [Get("/api/Movie")]
-    Task<List<MovieModel>> GetMovies();
+    public interface IMovieAPI
+    {
+        [Get("/api/Movie")]
+        Task<List<MovieModel>> GetMovies();
 
-    [Get("/api/Movie/{id}")]
-    Task<MovieModel> GetMovie(int id);
+        [Get("/api/Movie/{id}")]
+        Task<MovieModel> GetMovie(int id);
 
-    [Post("/api/Movie")]
-    Task<string> CreateMovie(MovieModel movie);
+        [Post("/api/Movie")]
+        Task<string> CreateMovie(MovieModel movie);
 
-    [Put("/api/Movie/{id}")]
-    Task<string> UpdateMovie(int id, MovieModel movie);
+        [Put("/api/Movie/{id}")]
+        Task<string> UpdateMovie(int id, MovieModel movie);
 
-    [Delete("/api/Movie/{id}")]
-    Task<string> DeleteMovie(int id);
+        [Delete("/api/Movie/{id}")]
+        Task<string> DeleteMovie(int id);
 
-}
+    }
 
-public class MovieModel
-{
+    public class MovieModel
+    {
     
-    public int MovieID { get; set; }
-    public string? MovieName { get; set; }
-    public string? MovieTitle { get; set; }
-    public string? MovieContent { get; set; }
+        public int MovieID { get; set; }
+        public string? MovieName { get; set; }
+        public string? MovieTitle { get; set; }
+        public string? MovieContent { get; set; }
 
+    }
 }
