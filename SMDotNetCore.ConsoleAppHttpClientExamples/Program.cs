6ad520b524d119c978de2bf24b4df14e84afe88a
sg788 <sumangtm1083@gmail.com>
2025-01-14 22:03:05 +0630

    -

--- SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
+++ SMDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -1,39 +1,38 @@
-﻿
-using Newtonsoft.Json;
-using SMDotNetCore.ConsoleAppHttpClientExamples;
-using System.ComponentModel.DataAnnotations;
+﻿using SMDotNetCore.ConsoleAppHttpClientExamples;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-//HttpClient client = new HttpClient();
+        //HttpClient client = new HttpClient();
 
-//{
-//    var response = await client.GetAsync("https://localhost:7294/api/MovieEFCore");
-//    if (response.IsSuccessStatusCode)
-//    {
-//        string jsonStr = await response.Content.ReadAsStringAsync();
-//        List<MovieModel> lst = JsonConvert.DeserializeObject<List<MovieModel>>(jsonStr)!;
+        //{
+        //    var response = await client.GetAsync("https://localhost:7294/api/MovieEFCore");
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string jsonStr = await response.Content.ReadAsStringAsync();
+        //        List<MovieModel> lst = JsonConvert.DeserializeObject<List<MovieModel>>(jsonStr)!;
 
-//        foreach (var movie in lst)
-//        {
-//            Console.WriteLine(JsonConvert.SerializeObject(movie));
-//            Console.WriteLine($"Name=>{movie.MovieName}");
-//            Console.WriteLine($"Title=>{movie.MovieTitle}");
-//            Console.WriteLine($"Content=>{movie.MovieContent}");
-//        }
-//    }
-//    else
-//    {
-//        Console.WriteLine("Failed to get data");
-//    }
-
-
-
-HttpClientExample httpClientExample = new HttpClientExample();
-await httpClientExample.RunAsync();
-
-Console.ReadLine();
+        //        foreach (var movie in lst)
+        //        {
+        //            Console.WriteLine(JsonConvert.SerializeObject(movie));
+        //            Console.WriteLine($"Name=>{movie.MovieName}");
+        //            Console.WriteLine($"Title=>{movie.MovieTitle}");
+        //            Console.WriteLine($"Content=>{movie.MovieContent}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Failed to get data");
+        //    }
 
 
 
+        HttpClientExample httpClientExample = new HttpClientExample();
+        await httpClientExample.RunAsync();
 
+        Console.ReadLine();
+    }
+}
