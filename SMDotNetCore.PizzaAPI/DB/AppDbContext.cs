b2b8347e7f7bbee1de6a763e79a5be86353efc5f
sg788 <sumangtm1083@gmail.com>
2025-01-15 20:19:15 +0630

    -

--- SMDotNetCore.PizzaAPI/DB/AppDbContext.cs
+++ SMDotNetCore.PizzaAPI/DB/AppDbContext.cs
@@ -1,41 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SMDotNetCore.PizzaAPI.Model;
 
-namespace SMDotNetCore.PizzaAPI.DB;
-
-internal class AppDbContext : DbContext
+namespace SMDotNetCore.PizzaAPI.DB
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    internal class AppDbContext : DbContext
     {
-        optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<PizzaModel> Pizzas { get; set; }
+
+        public DbSet<PizzaExtraModel> PizzaExtras { get; set; }
+
+        public DbSet<PizzaOrderModel> PizzaOrders { get; set; }
+        public DbSet<PizzaOrderDetailModel> PizzaOrderDetails { get; set; }
+
     }
 
-    public DbSet<PizzaModel> Pizzas { get; set; }
+    public class OrderRequest
+    {
+        public int PizzaId { get; set; }
+        public int[] Extras { get; set; }
+    }
 
-    public DbSet<PizzaExtraModel> PizzaExtras { get; set; }
+    public class OrderResponse
+    {
+        public string Message { get; set; }
+        public string InvoiceNo { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
 
-    public DbSet<PizzaOrderModel> PizzaOrders { get; set; }
-    public DbSet<PizzaOrderDetailModel> PizzaOrderDetails { get; set; }
-
-
-
-}
-
-public class OrderRequest
-{
-    public int PizzaId { get; set; }
-    public int[] Extras { get; set; }
-}
-
-public class OrderResponse
-{
-    public string Message { get; set; }
-    public string InvoiceNo { get; set; }
-    public decimal TotalAmount { get; set; }
-}
-
-public class  PizzaOrderInvoiceResponse
-{
-    public PizzaOrderInvoiceHeadModel Order { get; set; }
-    public List<PizzaOrderInvoiceDetailModel> OrderDetail { get; set; }
+    public class  PizzaOrderInvoiceResponse
+    {
+        public PizzaOrderInvoiceHeadModel Order { get; set; }
+        public List<PizzaOrderInvoiceDetailModel> OrderDetail { get; set; }
+    }
 }
